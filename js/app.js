1e51b75d1a874987a3bff0ba029d2eb3bd01c6dd
Lusegar <lucas.seggar@epitech.eu>
2023-11-21 15:12:48 +0100

    [MODIF] add dynamisme

--- js/app.js
+++ js/app.js
@@ -1,8 +1,15 @@
-import { Description } from "./template.js";
-import { Template } from "./template.js";
+//import { msg } from "./app_init.js";
+import { Template } from "./app_init.js";
 
 document.addEventListener("DOMContentLoaded", e => {
+
     console.log(e.target);
-    console.table(Description);
     Template.printF();
+
+    /* console.log(msg) */
+    /* const json = JSON.stringify(City)
+    console.log(json+"\n")
+      for(let index in City){
+    console.log(index+" "+City[index]+'\n')
+    } */
 });
